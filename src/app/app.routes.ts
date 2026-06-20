e7c17529af06ca63186f0c855de0fc9a03486f5b
bicky raj kayastha <bickyrajkayastha@gmail.com>
2026-06-20 21:31:05 +0200

    initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,15 +5,23 @@ import {FrontendRoleEnum} from './utils/FrontendRoleEnum';
 import {ErrorLayoutComponent} from './layout/error-layout/error-layout.component';
 import {ForbiddenComponent} from './forbidden/forbidden.component';
 import {NotFoundComponent} from './not-found/not-found.component';
+import {RealmRoleEnum} from './utils/RealmRoleEnum';
+import {AdminLayoutComponent} from './layout/admin-layout/admin-layout.component';
 
 export const routes: Routes = [
   {
-    path: 'dashboard',
-    component: Dashboard,
-    canActivate: [canActivateAuthRole],
-    data: {
-      roles: [FrontendRoleEnum.USER, FrontendRoleEnum.SUPERADMIN]
-    }
+    path: '',
+    component: AdminLayoutComponent,
+    children: [
+      {
+        path: 'dashboard',
+        component: Dashboard,
+        canActivate: [canActivateAuthRole],
+        data: {
+          roles: [FrontendRoleEnum.USER, FrontendRoleEnum.SUPERADMIN]
+        }
+      },
+    ]
   },
   {
     path: '',
