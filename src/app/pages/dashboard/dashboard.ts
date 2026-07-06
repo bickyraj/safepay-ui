e8730a90851d93959c467d1a7773709a0c34819f
bicky raj kayastha <bickyrajkayastha@gmail.com>
2026-07-07 00:36:00 +0200

    updats

--- src/app/pages/dashboard/dashboard.ts
+++ src/app/pages/dashboard/dashboard.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
+import { AuthService } from '../../services/auth/auth.service';
 
 @Component({
   selector: 'app-dashboard',
   imports: [],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss',
+  standalone: true
 })
 export class Dashboard {
+
+  private readonly AuthService = inject(AuthService);
+
+  public readonly userName: string = this.AuthService.getUserName();
 
 }
